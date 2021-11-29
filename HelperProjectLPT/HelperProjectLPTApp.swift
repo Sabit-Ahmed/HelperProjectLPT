e44fa14650642b760c18b62dd004dd15509a3aa5
Sabit Ahmed <sabit.a.sirat@gmail.com>
2021-11-29 19:50:02 +0600

    Initial Commit

---
+++ HelperProjectLPT/HelperProjectLPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelperProjectLPTApp.swift
+//  HelperProjectLPT
+//
+//  Created by Sabit Ahmed on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct HelperProjectLPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
