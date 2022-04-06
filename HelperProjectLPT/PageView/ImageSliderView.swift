fab0378119185784400a11df50271ce9049fe9e1
Sabit Ahmed <sabit.a.sirat@gmail.com>
2022-04-06 15:42:45 +0600

    add url image for image slider images

--- HelperProjectLPT/PageView/ImageSliderView.swift
+++ HelperProjectLPT/PageView/ImageSliderView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import URLImage
 
 struct ImageSliderView: View {
 // 1
-    private let images = ["1", "2", "3", "4"]
+    private let images = ["https://mmhva.s3.amazonaws.com/Images%2fRS%2fEvalExerciseList%2fafa2cc85-83a2-49f9-aae6-14cf577b7e5e.jpeg", "https://mmhva.s3.amazonaws.com/Images%2fRS%2fEvalExerciseList%2ff940338f-6f4e-423d-a545-43e3f5e89721.jpeg",
+        "https://mmhva.s3.amazonaws.com/Images%2fRS%2fEvalExerciseList%2f9d0a8993-65c3-4349-89fa-f770f6e515e4.jpeg",
+        "https://mmhva.s3.amazonaws.com/Images%2fRS%2fEvalExerciseList%2f8c3a569f-4e0b-4eaa-b167-b315e74e3f8e.jpeg"]
     var deviceWidth = UIScreen.main.bounds.width
     @EnvironmentObject var apiData: GetApiData
     
@@ -24,9 +27,13 @@ struct ImageSliderView: View {
                 TabView {
                     ForEach(images, id: \.self) { item in
                          //3
-                         Image(item)
-                            .resizable()
-                            .scaledToFill()
+                        URLImage(URL(string: item)!) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 200, height: 150, alignment: .center)
+                                .cornerRadius(5)
+                        }
                             
                     }
                 }
